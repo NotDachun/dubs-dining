4282de97210ccd4872741ada895e022087b042c0
Dachun <Dzhu99@yahoo.com>
2018-10-21 04:12:48 -0700

    local version is lit

--- old/dining_visualizer/models.py
+++ old/dining_visualizer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    date_posted = models.DateTimeField()
+    change = models.DecimalField(max_digits=6, decimal_places=2)
+    balance = models.DecimalField(max_digits=6, decimal_places=2)
+    location = models.TextField()
--- old/dining_visualizer/views.py
+++ old/dining_visualizer/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    # return HttpResponse('<h1>hi</h1>')
     return render(request, 'dining_visualizer/firstPage.html')
 
 def visualization(request):
-    return render(request, 'dining_visualizer/visualization.html')
+    return render(request, 'dining_visualizer/visualization.html', {'title': 'Visualization'})
